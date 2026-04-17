d3a5f3e2123161bf9240e877aac0edd4be458d36
Abd Elhadi <mohamezarifi@gmail.com>
2026-04-17 12:44:29 -0400

    the villest3

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,14 @@ import cors from "cors";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.use(cors({
+
+  app.enableCors({
     origin: "http://localhost:5173",
-    credentials: true
-  }));
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
