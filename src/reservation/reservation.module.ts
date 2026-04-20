0991a5c5485e00cf2d4a7a86049b9250cfeb25eb
Rebahi M'hamed Wassim <wassimrbh00@gmail.com>
2026-04-20 16:37:12 +0200

    Add DailyService to ReservationModule
    Added DailyService to providers and exports in ReservationModule.

--- src/reservation/reservation.module.ts
+++ src/reservation/reservation.module.ts
@@ -1,14 +1,15 @@
-// reservation.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReservationService } from './reservation.service';
 import { ReservationController } from './reservation.controller';
 import { Reservation } from './reservation.entity';
 import { docschedule } from './schedule.entity';
+import { DailyService } from '../services/daily.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Reservation, docschedule])],
-    controllers: [ReservationController],
-    providers: [ReservationService],
-  })
-  export class ReservationModule {}
+  imports: [TypeOrmModule.forFeature([Reservation, docschedule])],
+  controllers: [ReservationController],
+  providers: [ReservationService, DailyService],
+  exports: [ReservationService, DailyService],
+})
+export class ReservationModule {}
