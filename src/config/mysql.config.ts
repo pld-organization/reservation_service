030cc638f419151ad141cf391a6e09122c724ebf
Rebahi M'hamed Wassim <wassimrbh00@gmail.com>
2026-04-20 18:33:24 +0200

    Update MySQL configuration structure
    Refactor MySQL configuration to include host, port, user, password, and SSL settings.

--- src/config/mysql.config.ts
+++ src/config/mysql.config.ts
@@ -1,5 +1,16 @@
-import { registerAs } from '@nestjs/config';
+import * as fs from 'fs';
 
-export default registerAs('mysql', () => ({
-  url: process.env.MYSQL_PUBLIC_URL,
-}));
+export const mysqlConfig = {
+  host: process.env.MYSQL_HOST,
+  port: parseInt(process.env.MYSQL_PORT || '11965'),
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  
+  ssl: {
+    ca: process.env.NODE_ENV === 'production'
+      ? fs.readFileSync('/etc/secrets/aiven-ca.pem', 'utf8')  // ✅ RENDER PATH
+      : fs.readFileSync('./ca.pem', 'utf8'),      // ✅ LOCAL PATH
+    rejectUnauthorized: true,
+  },
+};
