4926460d00085fdad894c6f0bf05818a08bf27c4
Rebahi M'hamed Wassim <wassimrbh00@gmail.com>
2026-04-20 19:48:21 +0200

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
   getHealth(): { status: string; message: string } {
     return {
       status: 'ok',
