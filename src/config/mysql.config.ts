2770457c97d6b9d6483050ac78b0aad285480459
Rebahi M'hamed Wassim <wassimrbh00@gmail.com>
2026-04-20 20:52:02 +0200

    Update mysql.config.ts

--- src/config/mysql.config.ts
+++ src/config/mysql.config.ts
@@ -1,20 +1,5 @@
-import * as fs from 'fs';
 import { registerAs } from '@nestjs/config';
 
-export const mysqlConfig = {
-  host: process.env.MYSQL_HOST,
-  port: parseInt(process.env.MYSQL_PORT || '11965'),
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  
-  ssl: {
-    ca: process.env.NODE_ENV === 'production'
-      ? fs.readFileSync('/etc/secrets/aiven-ca.pem', 'utf8')  // Render path
-      : fs.readFileSync('./ca.pem', 'utf8'),                  // Local path
-    rejectUnauthorized: true,
-  },
-};
-
-// ✅ AJOUTEZ CE CI - export default
-export default registerAs('mysql', () => mysqlConfig);
+export default registerAs('mysql', () => ({
+  url: process.env.MYSQL_PUBLIC_URL,
+}));
